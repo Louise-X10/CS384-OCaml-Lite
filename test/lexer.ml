18fcf7bb4f90cdd338a6be78c5c934ef88c0aeac
LouiseX-10 <louisexu426@gmail.com>
2023-10-27 13:16:34 -0700

    add tests

--- test/lexer.ml
+++ test/lexer.ml
@@ -1,5 +1,7 @@
 open OUnit2
 open Ocaml_lite.Lexer
+open Ocaml_lite.Ast
+open Ocaml_lite.Parser
 
 let lex_tests = "test suite for tokenize" >::: [
     "random code" >::
@@ -24,4 +26,272 @@ let lex_tests = "test suite for tokenize" >::: [
 
     "number id" >::
     (fun _ -> assert_equal [Int 32; Id "xyz"] (tokenize "32xyz"));
+  ]
+
+let parse_tests = "test suite for parser" >::: [
+    "arithmetic computations" >::
+    (fun _ -> assert_equal
+        (Mod(
+          Sub(
+            Add(
+              Div(CInt 1, CInt 2), 
+              Mul(CInt 3, CInt 4)),
+            CInt 5),
+          CInt 6)
+        )
+        (parse " (1 / 2 + 3 * 4 - 4 ) mod 6"));
+
+    "comparisons" >::
+    (fun _ -> assert_equal
+        (Equal(Lt(Add(CInt 2, CInt 3), CInt 7), CBool true))
+        (parse " 2 + 3 < 7 = true"));
+
+    "logic expressions" >::
+    (fun _ -> assert_equal
+        (Or(And(Negate(CBool true), CBool false), CBool true))
+        (parse "not true && false || true"));
+
+    "string concat" >::
+    (fun _ -> assert_equal
+        (Concat(CString "hello", CString "world"))
+        (parse
+          " \"hello\" ^ \"world\" "));
+
+    "integer negate" >::
+    (fun _ -> assert_equal
+        (Add(IntNegate (CInt 1), CInt 2))
+        (parse "~1 + 2"));
+
+    "if expression" >::
+      (fun _ -> assert_equal
+          (If(Lt(CInt 2, CInt 3), CBool true, CBool false))
+          (parse  "if 2 < 3 then true else false"));
+
+    "let expression, with type" >::
+      (fun _ -> assert_equal
+          (LetExp("x", [], Some IntTy, Add(Id "x", CInt 1)))
+          (parse  "let x : int = 0 in x + 1"));
+
+    "let expression, no type" >::
+    (fun _ -> assert_equal
+        (LetExp("x", [], None, Add(Id "x", CInt 1)))
+        (parse  "let x = 0 in x + 1"));
+
+    "let binding, with type" >::
+    (fun _ -> assert_equal
+        (
+          LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
+          If(Lt(Id "x", CInt 0), CBool true, CBool false))
+        )
+        (parse
+            "let f (x : int) : bool = if x < 0 then true else false;;"));
+
+    "function application from let binding, no type" >::
+    (fun _ -> assert_equal
+        ([
+          LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
+          If(Lt(Id "x", CInt 0), CBool true, CBool false)),
+          LetB("result", [], None, App (Id "f", CInt 1))
+        ])
+        (interpret
+            "let f (x : int) : bool = if x < 0 then true else false;;
+            let result = f 1;;"));
+
+    "function application from helper let function, no type" >::
+    (fun _ -> assert_equal
+        (
+        LetExp("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy,If(Lt(Id "x", CInt 0), CBool true, CBool false)),
+        App (Id "f", CInt 1)
+        )
+        (interpret
+            "let f (x : int) : bool = if x < 0 then true else false in f 1"));
+            
+    "match expression" >::
+    (fun _ -> assert_equal
+        (MatchExp(Id "p", [
+          MatchBr(Id "Pair", Some ["x", "y"], Add (Id "x", Id "y"))
+        ]))
+        (parse  "match p with | Pair (x, y) => x + y"));
+
+    "function expression, no type" >::
+    (fun _ -> assert_equal
+        (
+        Fun([{ name="x"; p_type=None}, { name="y"; p_type=None}], Some IntTy, 
+        Add(Id "x", Mul(CInt 2, Id "y")))  
+        )
+        (parse  "fun x y : int => x + 2y"));
+
+    "function expression, with type" >::
+    (fun _ -> assert_equal
+        (
+        Fun([{ name="x"; p_type=None}, { name="y"; p_type=None}], Some IntTy, 
+        Add(Id "x", Mul(CInt 2, Id "y")))  
+        )
+        (parse  "fun (x:int) (y:int) : int => x + 2y"));
+
+    "type binding" >::
+    (fun _ -> assert_equal
+        (
+        TypeB("pairing", ["Pair", TupleTy(IntTy, IntTy)])
+        )
+        (parse  "type pairing = | Pair of int * int"));
+  ]
+
+let type_tests = "test suite for typechecker" >::: [
+
+  "built-in int_of_string" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (FuncTy (IntTy, StringTy))
+      (typecheck (parse "int_of_string")));
+
+  "built-in string_of_int" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (FuncTy (StringTy, IntTy))
+      (typecheck (parse "string_of_int")));
+
+  "built-in print_string" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (FuncTy (StringTy, UnitTy))
+      (typecheck (parse "print_string")));
+
+  "id function" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (FuncTy (IntTy, IntTy))
+      (typecheck (parse "fun x : int => x")));
+
+  "add function" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (FuncTy (TupleTy(IntTy, IntTy), IntTy))
+      (typecheck (parse "fun x y : int => x + y")));
+
+  "application" >::
+  (fun _ -> assert_equal ~printer:typ_to_str
+      (IntTy)
+      (typecheck (parse "let f = fun x y : int => x + 2y in f 1 2")));
+
+  "application convoluted" >::
+      (fun _ -> assert_equal ~printer:typ_to_str
+          (FuncTy( FuncTy(IntTy, IntTy), FuncTy(IntTy, IntTy) ))
+          (typecheck (parse "
+          let f = fun x : int => 2x in
+          fun f => (fun a : int => (f a))")));
+
+  "unit type" >::
+      (fun _ -> assert_equal ~printer:typ_to_str
+          (UnitTy)
+          (typecheck (parse "print_string \"hello\" ")));
+
+  "ill-typed" >::
+      (fun _ -> try
+          let _ = typecheck (parse "
+          let f = fun (x:int) => (fun (y:bool) => ()) in f 3 ()") in
+          assert_failure "'let f = fun (x:int) => (fun (y:bool) => ()) in f 3 ()' passed the typechecker"
+      with
+      | TypeError _ -> assert_bool "" true
+      | _ -> assert_failure "Unexpected error");
+
+  "apply inside lambda" >::
+      (fun _ -> try
+          let _ = typecheck (parse "fun x => x y") in
+          assert_failure "'fun x => x y' passed the typechecker"
+      with
+      | TypeError _ -> assert_bool "" true
+      | _ -> assert_failure "Unexpected error");
+  ]
+
+let interp_tests = "test suite for interpretor" >::: [
+  "arithmetic computations" >::
+  (fun _ -> assert_equal
+      (CInt 2)
+      (interpret " (1 / 2 + 3 * 4 - 4 ) mod 6"));
+
+  "comparisons" >::
+  (fun _ -> assert_equal
+      (CBool true)
+      (interpret " 2 + 3 < 7 = true"));
+
+  "logic expressions" >::
+  (fun _ -> assert_equal
+      (CBool true)
+      (interpret "not true && false || true"));
+
+  "string concat" >::
+  (fun _ -> assert_equal
+      (CString "helloworld")
+      (interpret " \"hello\" ^ \"world\" "));
+
+  "integer negate" >::
+  (fun _ -> assert_equal
+      (CInt 1)
+      (interpret  "~1 + 2"));
+
+  "if expression" >::
+  (fun _ -> assert_equal
+      (CBool true)
+      (interpret  "if 2 < 3 then true else false"));
+
+  "let expression, with type" >::
+  (fun _ -> assert_equal
+      (CInt 1)
+      (interpret  "let x : int = 0 in x + 1"));
+
+  "let expression, no type" >::
+  (fun _ -> assert_equal
+      (CInt 1)
+      (interpret  "let x = 0 in x + 1"));
+
+  "let binding, with type" >::
+  (fun _ -> assert_equal
+      (
+        LetB("f", [{ name="x"; p_type=None}], Some BoolTy, 
+        If(Lt(Id "x", CInt 0), CBool true, CBool false))
+      )
+      (interpret
+          "let f (x : int) : bool = if x < 0 then true else false;;"));
+
+  "function application from let binding, no type" >::
+  (fun _ -> assert_equal
+      ([
+        LetB("f", [{ name="x"; p_type=None}], Some BoolTy, 
+        If(Lt(Id "x", CInt 0), CBool true, CBool false)),
+        LetB("result", [], Some BoolTy, CBool true)
+      ])
+      (interpret
+          "let f (x : int) : bool = if x < 0 then true else false;;
+          let result = f 1;;"));
+
+  "function application from let binding, no type" >::
+  (fun _ -> assert_equal
+      (CBool true)
+      (interpret
+          "let f (x : int) : bool = if x < 0 then true else false in f 1"));
+
+  "match expression" >::
+  (fun _ -> assert_equal
+      ([
+        TypeB("pairing", ["Pair", TupleTy(IntTy, IntTy)]),
+        LetB("p", [], Some (CustomTy "pairing"), App( App(Id "Pair", CInt 1), CInt 2)),
+        LetB("result", [], Some IntTy, CInt 3)
+      ])
+      (interpret  "
+      type pairing = | Pair of int * int;;
+      let p : pairing = Pair (1, 2);;
+      let result = match p with | Pair (x, y) => x + y;;"));
+
+  "function expression, no type" >::
+  (fun _ -> assert_equal
+      (
+      Fun([{ name="x"; p_type=Some IntTy}, { name="y"; p_type=Some IntTy}], Some IntTy, 
+      Add(Id "x", Mul(CInt 2, Id "y")))  
+      )
+      (parse  "fun x y : int => x + 2y"));
+
+  "function expression, with type" >::
+  (fun _ -> assert_equal
+      (
+      Fun([{ name="x"; p_type=Some IntTy}, { name="y"; p_type=Some IntTy}], Some IntTy, 
+      Add(Id "x", Mul(CInt 2, Id "y")))  
+      )
+      (parse  "fun (x:int) (y:int) : int => x + 2y"));
+    
   ]
