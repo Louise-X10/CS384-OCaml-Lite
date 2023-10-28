45b6d822da1eba96dcdeaabcde3b6741ce742138
LouiseX-10 <louisexu426@gmail.com>
2023-10-27 23:27:43 -0700

    fix parse test format

--- test/lexer.ml
+++ test/lexer.ml
@@ -31,124 +31,145 @@ let lex_tests = "test suite for tokenize" >::: [
 let parse_tests = "test suite for parser" >::: [
     "unit" >::
     (fun _ -> assert_equal
-        (LetB("x", [], None, Unit)
+        ([LetB("res", [], None, Unit)]
         )
-        (parse "let x = ();;"));
+        (parse "let res = ();;"));
 
     "tuple" >::
     (fun _ -> assert_equal
-        (LetB("x", [], None, Tuple([CInt 1;CInt 2;CInt 3]))
-        )
-        (parse "let x = (1, 2, 3);;"));
+        ([LetB("res", [], None, Tuple([CInt 1;CInt 2;CInt 3]))])
+        (parse "let res = (1, 2, 3);;"));
 
 
     "arithmetic computations" >::
     (fun _ -> assert_equal
-        (LetB("x", [], None, 
+        ([LetB("res", [], None, 
             Modulo(
                 Sub(
                     Add(
                     Div(CInt 1, CInt 2), 
                     Mul(CInt 3, CInt 4)),
                 CInt 5),
-            CInt 6))
+            CInt 6))]
         )
-        (parse "let x = (1 / 2 + 3 * 4 - 4 ) mod 6;;"));
+        (parse "let res = (1 / 2 + 3 * 4 - 4 ) mod 6;;"));
 
     "comparisons" >::
     (fun _ -> assert_equal
-        (Equal(LessThan(Add(CInt 2, CInt 3), CInt 7), CBool true))
-        (parse " 2 + 3 < 7 = true"));
+        ([LetB("res", [], None, 
+            Equal(LessThan(Add(CInt 2, CInt 3), CInt 7), CBool true)
+        )])
+        (parse "let res = 2 + 3 < 7 = true;;"));
 
     "logic expressions" >::
     (fun _ -> assert_equal
-        (LogicOr(LogicAnd(LogicNegate(CBool true), CBool false), CBool true))
-        (parse "not true && false || true"));
+        ([LetB("res", [], None, 
+        LogicOr(LogicAnd(LogicNegate(CBool true), CBool false), CBool true))])
+        (parse "let res = not true && false || true"));
 
     "string concat" >::
     (fun _ -> assert_equal
-        (StrConcat(CString "hello", CString "world"))
+        ([LetB("res", [], None, 
+        StrConcat(CString "hello", CString "world"))])
         (parse
-          " \"hello\" ^ \"world\" "));
+          " let res = \"hello\" ^ \"world\";; "));
 
     "integer negate" >::
     (fun _ -> assert_equal
-        (Add(IntNegate (CInt 1), CInt 2))
-        (parse "~1 + 2"));
+        ([LetB("res", [], None, 
+        Add(IntNegate (CInt 1), CInt 2))])
+        (parse "let res = ~1 + 2;;"));
 
     "if expression" >::
       (fun _ -> assert_equal
-          (If(LessThan(CInt 2, CInt 3), CBool true, CBool false))
-          (parse  "if 2 < 3 then true else false"));
+          ([LetB("res", [], None, 
+          If(LessThan(CInt 2, CInt 3), CBool true, CBool false))])
+          (parse  "let res = if 2 < 3 then true else false;;"));
 
     "let expression, with type" >::
       (fun _ -> assert_equal
-          (LetExp("x", [], Some IntTy, Add(Var "x", CInt 1)))
-          (parse  "let x : int = 0 in x + 1"));
+          ([LetB("res", [], None, 
+          LetExp("x", [], Some IntTy, CInt 0, Add(Var "x", CInt 1)))])
+          (parse  "let res = let x : int = 0 in x + 1;;"));
 
     "let expression, no type" >::
     (fun _ -> assert_equal
-        (LetExp("x", [], None, Add(Var "x", CInt 1)))
-        (parse  "let x = 0 in x + 1"));
+        ([LetB("res", [], None, 
+        LetExp("x", [], None, CInt 0, Add(Var "x", CInt 1)))])
+        (parse  "let res = let x = 0 in x + 1;;"));
 
     "let binding, with type" >::
     (fun _ -> assert_equal
         (
-          LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
-          If(LessThan(Var "x", CInt 0), CBool true, CBool false))
+          [LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
+          If(LessThan(Var "x", CInt 0), CBool true, CBool false))]
         )
         (parse
             "let f (x : int) : bool = if x < 0 then true else false;;"));
 
     "function application from let binding, no type" >::
     (fun _ -> assert_equal
+        ([LetB("f", [{ name="x"; p_type=None}], None, 
+            If(LessThan(Var "x", CInt 0), CBool true, CBool false));
+         LetB("result", [], None, App (Var "f", CInt 1))
+        ])
+        (parse
+            "let f x = if x < 0 then true else false;;
+            let result = f 1;;"));
+
+    
+    "function application from let binding, with type" >::
+    (fun _ -> assert_equal
         ([
           LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
-          If(LessThan(Var "x", CInt 0), CBool true, CBool false)),
+          If(LessThan(Var "x", CInt 0), CBool true, CBool false));
           LetB("result", [], None, App (Var "f", CInt 1))
         ])
-        (interpret
+        (parse
             "let f (x : int) : bool = if x < 0 then true else false;;
             let result = f 1;;"));
 
     "function application from helper let function, no type" >::
     (fun _ -> assert_equal
-        (
-        LetExp("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy,If(LessThan(Var "x", CInt 0), CBool true, CBool false)),
-        App (Var "f", CInt 1)
+        ([LetB("res", [], None, 
+            LetExp("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy,
+            If(LessThan(Var "x", CInt 0), CBool true, CBool false),
+            App (Var "f", CInt 1))
+        )]
         )
-        (interpret
-            "let f (x : int) : bool = if x < 0 then true else false in f 1"));
+        (parse
+            "let res = let f (x : int) : bool = if x < 0 then true else false in f 1;;"));
             
     "match expression" >::
     (fun _ -> assert_equal
-        (MatchExp(Var "p", [
-          MatchBr(Var "Pair", Some ["x", "y"], Add (Var "x", Var "y"))
-        ]))
-        (parse  "match p with | Pair (x, y) => x + y"));
+        ([LetB("res", [], None, 
+            MatchExp(Var "p", [
+            MatchBr("Pair", Some ["x"; "y"], Add (Var "x", Var "y"))]))
+        ])
+        (parse  "let res = match p with | Pair (x, y) => x + y;;"));
 
     "function expression, no type" >::
     (fun _ -> assert_equal
-        (
-        Fun([{ name="x"; p_type=None}, { name="y"; p_type=None}], Some IntTy, 
-        Add(Var "x", Mul(CInt 2, Var "y")))  
-        )
-        (parse  "fun x y : int => x + 2y"));
+        ([LetB("res", [], None, 
+            Fun([{ name="x"; p_type=None }; { name="y"; p_type=None }], Some IntTy, 
+            Add(Var "x", Mul(CInt 2, Var "y"))))
+        ])
+        (parse  "let res = fun x y : int => x + 2y;;"));
 
     "function expression, with type" >::
     (fun _ -> assert_equal
-        (
-        Fun([{ name="x"; p_type=None}, { name="y"; p_type=None}], Some IntTy, 
-        Add(Var "x", Mul(CInt 2, Var "y")))  
-        )
-        (parse  "fun (x:int) (y:int) : int => x + 2y"));
+        ([LetB("res", [], None, 
+            Fun([{ name="x"; p_type=None}; { name="y"; p_type=None}], Some IntTy, 
+            Add(Var "x", Mul(CInt 2, Var "y"))))  
+        ])
+        (parse  "let res = fun (x:int) (y:int) : int => x + 2y;;"));
 
     "type binding" >::
     (fun _ -> assert_equal
-        (
-        TypeB("pairing", ["Pair", TupleTy(IntTy, IntTy)])
-        )
-        (parse  "type pairing = | Pair of int * int"));
+        ([TypeB("pairing", [
+            ("Pair", Some (TupleTy (IntTy, IntTy)))
+        ])])
+        (parse  "type pairing = | Pair of int * int;;"));
   ]
 
 let type_tests = "test suite for typechecker" >::: [
@@ -266,15 +287,26 @@ let interp_tests = "test suite for interpretor" >::: [
   "function application from let binding, no type" >::
   (fun _ -> assert_equal
       ([
-        LetB("f", [{ name="x"; p_type=None}], Some BoolTy, 
+        LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
         If(LessThan(Var "x", CInt 0), CBool true, CBool false)),
         LetB("result", [], Some BoolTy, CBool true)
       ])
       (interpret
-          "let f (x : int) : bool = if x < 0 then true else false;;
+          "let f x = if x < 0 then true else false;;
           let result = f 1;;"));
 
-  "function application from let binding, no type" >::
+    "function application from let binding, with type" >::
+    (fun _ -> assert_equal
+        ([
+        LetB("f", [{ name="x"; p_type=Some IntTy}], Some BoolTy, 
+        If(LessThan(Var "x", CInt 0), CBool true, CBool false)),
+        LetB("result", [], Some BoolTy, CBool true)
+        ])
+        (interpret
+            "let f (x : int) : bool = if x < 0 then true else false;;
+            let result = f 1;;"));
+
+  "function application from let binding, with type" >::
   (fun _ -> assert_equal
       (CBool true)
       (interpret
