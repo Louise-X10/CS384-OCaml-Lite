6d480e990fd4e6915bb6ca44aaa2e2f67f195f28
LouiseX-10 <louisexu426@gmail.com>
2023-11-17 11:45:42 -0800

    Add unify, fix merge clst

--- lib/typechecker.ml
+++ lib/typechecker.ml
@@ -38,16 +38,97 @@ end
 
 module TypeChecker = struct
   type constr = typ * typ (* e.g. (t0, int) *)
-  type context = string * typ (* e.g. (x, t0) *)
+  type context = string * typ (* e.g. (x, t0) means x bound to t0 *)
   type constr_state = {clst: constr list; context: context list}
   module ConstrState = State(struct type state = constr_state end)
+  type sub = typ * typ (* e.g. (t0, int) means map t0 to int *)
   open ConstrState
 
   (* Generate new type variables *)
   let next_var = ref 0
   let fresh_var (_ : unit) : typ =
     let () = next_var := !next_var + 1 in
-    CustomTy ("t$" ^ string_of_int !next_var)
+    VarTy (!next_var)
+
+  (* Check if t1 occurs in t2 *)
+  let rec occurs_check (t1: typ) (t2: typ): bool = 
+    match t2 with 
+    | FuncTy(arg, body) -> 
+      if t1 == arg then true else occurs_check t1 body
+    | TupleTy tlst -> List.exists (fun t -> occurs_check t1 t) tlst
+    | CustomTy _ as t -> if t1 == t then true else false
+    | _ -> false
+
+  (* Helper: apply sub (i.e. map any occurance of t1 to t2) in target_t*)
+  let rec sub_type (subst: sub) (target_t: typ): typ = 
+    match target_t with 
+    | FuncTy(arg, body) -> 
+      let new_arg = (if arg = fst subst then snd subst else arg) in 
+      let new_body = sub_type subst body in
+      FuncTy(new_arg, new_body)
+    | TupleTy tlst -> TupleTy (List.map (fun t -> sub_type subst t) tlst)
+    | CustomTy _ as t -> if t == fst subst then snd subst else t
+    | t -> t
+
+  (* Helper: apply single sub to single constraint *)
+  let sub_type_in_constr (subst: sub) (c: constr): constr = 
+    let t1 = fst c in 
+    let t2 = snd c in 
+    sub_type subst t1, sub_type subst t2
+
+  (* Helper: apply single sub to constraint list *)
+  let sub_type_all_constrts (clst: constr list) (subst:sub): constr list = 
+    List.map (fun c -> sub_type_in_constr subst c) clst
+
+  (* Helper: unify single constraint, return list of new constraints or new substitutions *)
+  let unify_single_constr: constr -> constr list * sub list = function
+    | VarTy t1, VarTy t2 -> 
+      if t1 <> t2 then [], [(VarTy t1, VarTy t2)]
+      else  [], []
+    | (VarTy _ as t1), t2 | t2, (VarTy _ as t1) -> 
+      if occurs_check t1 t2 == true then failwith "Infinite unification" else
+      [], [(t1, t2)]
+    | FuncTy (arg1, body1), FuncTy (arg2, body2) -> 
+      [(arg1, arg2); (body1, body2)], []
+    | TupleTy tls1, TupleTy tls2 -> 
+      List.combine tls1 tls2, []
+    | t1, t2 -> if t1 <> t2 then failwith "Unification failed" else [], []
+  
+  (* Helper: unify all constraints to build sub list *)
+  let rec unify_multiple_constr (clst: constr list) (sub: sub list): constr list * sub list = 
+    match clst with 
+    (* stop if clst is empty *)
+    | [] -> [], sub
+    (* apply first constraint, recurse on remaining clst *)
+    | (t1, t2) :: tl_clst -> 
+    let new_constr, new_sub = unify_single_constr (t1, t2) in
+    let sub = new_sub @ sub in
+    let clst = new_constr @ tl_clst in 
+    (* apply new_sub to all constraints *) 
+    let clst = if new_sub <> [] then (List.fold_left sub_type_all_constrts clst new_sub) else clst in 
+    unify_multiple_constr clst sub
+    
+  (* Helper: apply list of substitutions to t once *)
+  let rec apply_subst_oneround (slst: sub list) (t: typ): typ = 
+    match slst with 
+    | [] -> t
+    | sub :: [] -> sub_type sub t
+    | sub :: tail -> 
+      let ret_t = sub_type sub t in 
+      apply_subst_oneround tail ret_t
+
+  (* Helper: apply list of substitutions to t until it doesn't change *)
+  let rec apply_subst (slst: sub list) (t: typ): typ = 
+    let init_t = t in 
+    let res_t = apply_subst_oneround slst t in 
+    if init_t = res_t then res_t else apply_subst slst res_t
+
+  (* Unify constraint list to give actual return type *)
+  let unify (clst: constr list) (t: typ): typ = 
+    let res_clst, res_slst = unify_multiple_constr clst [] in 
+    let _ = match res_clst with | [] -> res_clst | _ -> failwith "Unresolved constraints!" in 
+    let ret_t = apply_subst res_slst t in 
+    ret_t
 
   (* Helper: Merge multiple context lists without duplicates *)
   let merge_context (lst_of_context: context list list)  = 
@@ -56,7 +137,7 @@ module TypeChecker = struct
     List.sort_uniq compare contexts
 
   (* Helper: Merge multiple constraint lists without duplicates *)
-  let merge_constr (lst_of_constr: constr list list)  = 
+  let merge_clst (lst_of_constr: constr list list)  = 
     (* let comp = (fun (x, x') (y, y') -> String.compare x y) in *)
     let constraints = List.fold_left (@) [] lst_of_constr in 
     List.sort_uniq compare constraints
@@ -110,11 +191,14 @@ module TypeChecker = struct
     | [] -> failwith "no function params"
     | (_, ti)::[]->  FuncTy(ti, ret_t)
     | (_, t1) :: tail -> FuncTy(t1, get_function_type tail ret_t)
+  
+  let empty_state = {clst = []; context = []} (*!! delete this later *)
   let rec typeof (e:expr) :typ = 
     let empty_state = {clst = []; context = []} in
     let st = typecheck e in 
-    (* let st = unify st in *)
-    eval_state st empty_state
+    let ret_t, ret_st = run_state st empty_state in 
+    let ret_t = unify ret_st.clst ret_t in 
+    ret_t
 
   and typecheck: expr -> typ ConstrState.m = function
 
@@ -147,36 +231,36 @@ module TypeChecker = struct
         get >>= fun initial_state ->
         let t1, st1 = run_state (typecheck e1) initial_state in
         let t2, st2 = run_state (typecheck e2) initial_state in
-        put {clst = [(t1, IntTy);(t2, IntTy)] @ st1.clst @ st2.clst ;
-            context = merge_context [st1.context; st2.context]} >>= fun _ ->
+        put {clst = merge_clst [[(t1, IntTy);(t2, IntTy)]; st1.clst; st2.clst] ;
+            context = initial_state.context} >>= fun _ ->
         return BoolTy
       | Equal -> 
         get >>= fun initial_state ->
         let t1, st1 = run_state (typecheck e1) initial_state in
         let t2, st2 = run_state (typecheck e2) initial_state in
-        put {clst = [(t1, t2)] @ st1.clst @ st2.clst ;
-        context = merge_context [st1.context; st2.context]} >>= fun _ ->
+        put {clst = merge_clst [[(t1, t2)]; st1.clst; st2.clst] ;
+        context = initial_state.context} >>= fun _ ->
         return BoolTy
       | StrConcat -> 
         get >>= fun initial_state ->
         let t1, st1 = run_state (typecheck e1) initial_state in
         let t2, st2 = run_state (typecheck e2) initial_state in
-        put {clst = [(t1, StringTy);(t2, StringTy)] @ st1.clst @ st2.clst ;
-            context = merge_context [st1.context; st2.context]} >>= fun _ ->
+        put {clst = merge_clst [[(t1, StringTy);(t2, StringTy)]; st1.clst; st2.clst] ;
+            context = initial_state.context} >>= fun _ ->
         return StringTy
       | LogicAnd | LogicOr -> 
         get >>= fun initial_state ->
         let t1, st1 = run_state (typecheck e1) initial_state in
         let t2, st2 = run_state (typecheck e2) initial_state in
-        put {clst = [(t1, BoolTy);(t2, BoolTy)] @ st1.clst @ st2.clst ;
-            context = merge_context [st1.context; st2.context]} >>= fun _ ->
+        put {clst = merge_clst [[(t1, BoolTy);(t2, BoolTy)]; st1.clst; st2.clst] ;
+            context = initial_state.context} >>= fun _ ->
         return BoolTy
       | _ -> 
         get >>= fun initial_state ->
         let t1, st1 = run_state (typecheck e1) initial_state in
         let t2, st2 = run_state (typecheck e2) initial_state in
-        put {clst = [(t1, IntTy);(t2, IntTy)] @ st1.clst @ st2.clst ;
-            context = merge_context [st1.context; st2.context]} >>= fun _ ->
+        put {clst = merge_clst [[(t1, IntTy);(t2, IntTy)]; st1.clst; st2.clst] ;
+            context = initial_state.context} >>= fun _ ->
         return IntTy
 
     and typecheck_unop (op: unop) (e: expr): typ ConstrState.m  = 
@@ -185,13 +269,13 @@ module TypeChecker = struct
         get >>= fun initial_state ->
         let t, st = run_state (typecheck e) initial_state in
         put {clst = (t,BoolTy) :: st.clst ;
-            context = st.context} >>= fun _ ->
+            context = initial_state.context} >>= fun _ ->
         return BoolTy
       | IntNegate -> 
         get >>= fun initial_state ->
         let t, st = run_state (typecheck e) initial_state in
         put {clst = (t,IntTy) :: st.clst ;
-            context = st.context} >>= fun _ ->
+            context = initial_state.context} >>= fun _ ->
         return IntTy
     and typecheck_tuple (elst: expr list) : typ ConstrState.m =
       get >>= fun initial_state ->
@@ -201,15 +285,15 @@ module TypeChecker = struct
         | e1 :: e2 :: [] -> 
           let t1, st1 = run_state (typecheck e1) initial_state in
           let t2, st2 = run_state (typecheck e2) initial_state in
-          [t1; t2] , merge_constr [st1.clst; st2.clst]
+          [t1; t2] , merge_clst [st1.clst; st2.clst]
         | e1 :: tail -> 
           let t1, st1 = run_state (typecheck e1) initial_state in
           let typs, new_constr = get_typs_constr tail in
-          t1 :: typs, merge_constr [st1.clst; new_constr]
+          t1 :: typs, merge_clst [st1.clst; new_constr]
         | _ -> failwith "not enough tuple elements"
         ) in
       let tlst, clst = get_typs_constr elst in 
-      put {clst = merge_constr [clst; initial_state.clst];
+      put {clst = merge_clst [clst; initial_state.clst];
           context = initial_state.context} >>= fun _ ->
         return (TupleTy tlst)
     and typecheck_ifexp (e1: expr) (e2: expr) (e3: expr): typ ConstrState.m  = 
@@ -218,16 +302,15 @@ module TypeChecker = struct
       let t2, st2 = run_state (typecheck e2) initial_state in
       let t3, st3 = run_state (typecheck e3) initial_state in
       put {clst = [(t1,BoolTy); (t2,t3)] @ st1.clst @ st2.clst @ st3.clst ;
-            context = merge_context [st1.context; st2.context; st3.context]} >>= fun _ ->
+            context = initial_state.context} >>= fun _ ->
         return t2
-
     and typecheck_app (e1: expr) (e2: expr): typ ConstrState.m  = 
       get >>= fun initial_state ->
       let t1, st1 = run_state (typecheck e1) initial_state in
       let t2, st2 = run_state (typecheck e2) initial_state in
       let t3 = fresh_var () in 
       put {clst = [(t1,FuncTy (t2, t3))] @ st1.clst @ st2.clst;
-            context = merge_context [st1.context; st2.context]} >>= fun _ ->
+            context = initial_state.context} >>= fun _ ->
         return t3
 
     (* Add params to context, then typecheck e, return ret_t with original context *)
@@ -262,7 +345,7 @@ module TypeChecker = struct
                               context = merge_context [pvar_context; match_state.context]} in 
           let ret_t, ret_st = run_state (typecheck e) branch_state in 
           (* return ret_typ of subexpr, restore initial context, update constraints *)
-          put {clst = merge_constr [ [new_constraint]; ret_st.clst];
+          put {clst = merge_clst [ [new_constraint]; ret_st.clst];
               context = init_state.context} >>= fun _ ->
           return  ret_t) in
       let st_lst = (List.map typecheck_matchbr brlst) in 
@@ -270,7 +353,7 @@ module TypeChecker = struct
       let list_of_clst = (List.map (fun st -> 
                             let res_st = exec_state st initial_state in res_st.clst) st_lst) in
       let subexpr_clst = get_subexpr_clst subexpr_tslt in 
-      put {clst = merge_constr (subexpr_clst :: list_of_clst);
+      put {clst = merge_clst (subexpr_clst :: list_of_clst);
           context = initial_state.context} >>= fun _ -> 
       return (List.hd subexpr_tslt)
 
